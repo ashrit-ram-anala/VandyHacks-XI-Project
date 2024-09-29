6dfae63c06cb624bfd75dab7b2e7572a460e64fc
Jesse Hao <jehyu77@gmail.com>
2024-09-29 09:55:51 -0500

    worked on search formatting and value logging

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -1,18 +1,35 @@
 import React from 'react'
 import axios from 'axios'
+import { useState } from "react";
+import "./Search.css"
 
 function Search() {
+    const [searchQuery, setSearchQuery] = useState("")
+
+    const handleChange = (event) => {
+        setSearchQuery(event.target.value); 
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        console.log(searchQuery)
+
+        // api stuff here i guess?
+    }
     
     return (
     <>
         <h1 className="header">hello?</h1>
-        <form>
-            <div>
-                <label className="tkr">Enter TKR here...</label>
-                bruh
-
-            </div>
-
+        <form onSubmit={handleSubmit}>
+            <label className="tkr">Enter TKR here: </label>
+            <br></br>
+            <input 
+            type="text" 
+            placeholder="Ex. AAPL, AMZN..." 
+            value={searchQuery}
+            onChange={handleChange}
+            />
+            <button type="submit">Search</button>
         </form>
 
 
