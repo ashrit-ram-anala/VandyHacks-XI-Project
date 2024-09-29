3fa2de8133fc4b254f4e30c9e145bd32fb62dd30
Ashrit-Anala <ashrit_ram@yahoo.com>
2024-09-29 00:30:54 -0500

    initialized tailwind and daisyUI

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const axios = require("axios");
 
 const app = express();
-const corsOptions = { origin: ["http://localhost:5173"] };
+const corsOptions = { origin: ["http://localhost:5173", "http://localhost:5000"] };
 const PORT = "8080";
 
 app.use(express.json());
@@ -49,14 +49,14 @@ app.post("/api/save-tkr", (req, res) => {
 
 app.get("/api/sentiment/:tkr", (req, res) => {
   const { tkr } = req.params;
-  const existingTkr = userModel.findOne({ tkr });
-  // .then((existingTkr) => {
+  userModel.findOne({ tkr })
+  .then((existingTkr) => {
   if (existingTkr) {
     res.json({ sentiment: existingTkr.sentiment });
   } else {
     res.status(404).json({ message: "Ticker not found" });
   }
-  // })
+   })
 });
 
 app.listen(PORT, () => {
